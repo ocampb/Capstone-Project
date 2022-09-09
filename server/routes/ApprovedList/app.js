572bae414ca822a5ea4c17a144267fa737da28ef
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-09-09 13:28:35 -0500

    connected to routes, reading all emails
    read all emails for primary key of user table

--- server/routes/ApprovedList/app.js
+++ server/routes/ApprovedList/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const { ApprovedList } = require("../../../models");
-const session = require("express-session"); //i am not sure yet if we will need this if we are able to store oauth presence in cookies
+const { ApprovedList, UsersTable } = require("../../../models");
+//const session = require("express-session"); //i am not sure yet if we will need this if we are able to store oauth presence in cookies
 const router = express.Router();
 
 //this is the function we will edit to check OAuth
@@ -15,10 +15,35 @@ const LoginCheck = async (req, res, next) => {
 };
 
 //render user's approved emails (will add login check when complete)
-router.get("/dashboard", async (req, res) => {
+router.get("/aaa", async (req, res) => {
   try {
     let array = [];
+    const { id } = req.body; //this will be whatever we need to get from OAuth to search for ID pKey in Users table
+    //it is req.params ^^^for now so i can test in postman
+    const findUser = await UsersTable.findOne({
+      where: {
+        id: id, //this will be switched to Calendly_ID once we have a login route
+      },
+    });
+    if (findUser) {
+      const findAll = await ApprovedList.findAll({
+        where: { User_ID: findUser.id },
+      });
+      if (findAll) {
+        for (let i = 0; i < findAll.length; i++) {
+          const findEach = await ApprovedList.findOne({
+            where: {
+              id: findAll[i].dataValues.id,
+            },
+          });
+          array.push(findEach.dataValues);
+        }
+        console.log(array);
+        res.status(200).send(array);
+      }
+    }
   } catch (error) {
     res.status(400).send(error);
   }
 });
+module.exports = router;
