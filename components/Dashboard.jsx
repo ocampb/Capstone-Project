a21ea85b43bd5f2d8649fe7865a86ab10b9f36fc
Amanda Hargrove <amandahargrove98@gmail.com>
2022-09-13 15:53:00 -0400

    Finished with modal window and popup delete styling

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -9,6 +9,9 @@ import Fade from "@mui/material/Fade";
 import Typography from "@mui/material/Typography";
 import Backdrop from "@mui/material/Backdrop";
 import Box from "@mui/material/Box";
+import CloseIcon from "@mui/icons-material/Close";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 // Styling for MUI modal window
 const style = {
@@ -16,7 +19,7 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  minWidth: 400,
   bgcolor: "background.paper",
   boxShadow: 24,
   p: 4,
@@ -26,9 +29,22 @@ const Dashboard = () => {
   // const dispatch = useDispatch();
   // const counter = useSelector((state) => state.counter);
 
+  //Open and close modal window
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  //Triple dot menu pop up to remove row
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const openDots = Boolean(anchorEl);
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleCloseDots = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div>
       {/* <h1>Counter: {counter}</h1>
@@ -38,9 +54,12 @@ const Dashboard = () => {
       <div className="dash-add">
         <h1>My Dashboard</h1>
 
-        <Button onClick={handleOpen} id="add-button-modal">
-          Add
-        </Button>
+        <input
+          type="button"
+          onClick={handleOpen}
+          id="add-button-modal"
+          value="Add"
+        />
         <Modal
           aria-labelledby="transition-modal-title"
           aria-describedby="transition-modal-description"
@@ -54,6 +73,10 @@ const Dashboard = () => {
         >
           <Fade in={open}>
             <Box sx={style}>
+              <div className="modal-close-icon">
+                <CloseIcon onClick={handleClose} sx={{ cursor: "pointer" }} />
+              </div>
+
               <Typography
                 id="transition-modal-title"
                 variant="h6"
@@ -66,8 +89,9 @@ const Dashboard = () => {
               <div className="add-input-flex">
                 <input type="text" placeholder="Name" />
                 <input type="email" placeholder="Email" />
-                <input type="text" placeholder="Notes" />
+                <textarea rows="4" cols="50" placeholder="Notes"></textarea>
               </div>
+
               <div className="submit-email">
                 <input
                   type="submit"
@@ -97,7 +121,26 @@ const Dashboard = () => {
               <td>sallyceo@gmail.com</td>
               <td>Is the CEO of the business</td>
               <td>
-                <MoreVertIcon />
+                <Button
+                  id="basic-button"
+                  aria-controls={openDots ? "basic-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={openDots ? "true" : undefined}
+                  onClick={handleClick}
+                >
+                  <MoreVertIcon />
+                </Button>
+                <Menu
+                  id="basic-menu"
+                  anchorEl={anchorEl}
+                  open={openDots}
+                  onClose={handleCloseDots}
+                  MenuListProps={{
+                    "aria-labelledby": "basic-button",
+                  }}
+                >
+                  <MenuItem onClick={handleCloseDots}>Delete</MenuItem>
+                </Menu>
               </td>
             </tr>
             <tr>
@@ -108,7 +151,26 @@ const Dashboard = () => {
                 take notes and ask any questions that you may have for him.
               </td>
               <td>
-                <MoreVertIcon />
+                <Button
+                  id="basic-button"
+                  aria-controls={openDots ? "basic-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={openDots ? "true" : undefined}
+                  onClick={handleClick}
+                >
+                  <MoreVertIcon />
+                </Button>
+                <Menu
+                  id="basic-menu"
+                  anchorEl={anchorEl}
+                  open={openDots}
+                  onClose={handleCloseDots}
+                  MenuListProps={{
+                    "aria-labelledby": "basic-button",
+                  }}
+                >
+                  <MenuItem onClick={handleCloseDots}>Delete</MenuItem>
+                </Menu>
               </td>
             </tr>
             <tr>
@@ -116,7 +178,26 @@ const Dashboard = () => {
               <td>karensales@gmail.com</td>
               <td>I mean, I guess she can join?</td>
               <td>
-                <MoreVertIcon />
+                <Button
+                  id="basic-button"
+                  aria-controls={openDots ? "basic-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={openDots ? "true" : undefined}
+                  onClick={handleClick}
+                >
+                  <MoreVertIcon />
+                </Button>
+                <Menu
+                  id="basic-menu"
+                  anchorEl={anchorEl}
+                  open={openDots}
+                  onClose={handleCloseDots}
+                  MenuListProps={{
+                    "aria-labelledby": "basic-button",
+                  }}
+                >
+                  <MenuItem onClick={handleCloseDots}>Delete</MenuItem>
+                </Menu>
               </td>
             </tr>
           </tbody>
