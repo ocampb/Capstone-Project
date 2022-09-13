c80a40cf795c3b2d07ca354e4a790d5db91bc120
Amanda Hargrove <amandahargrove98@gmail.com>
2022-09-13 16:46:13 -0400

    Made very basic layout for sign up page

--- components/Signup.jsx
+++ components/Signup.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "./styles/Signup.scss";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div>
+      <div className="layout-container">
+        <div className="signup-container">
+          <h1>Create an Account</h1>
+          <div className="form-container">
+            <form action="">
+              <input type="text" placeholder="Username" />
+              <input type="email" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+            </form>
+          </div>
+        </div>
+        <div className="login-link-container">
+          <div className="login-text-container">
+            <h3>Already have an account?</h3>
+            <a href="#">Login</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
