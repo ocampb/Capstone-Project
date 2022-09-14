2c849bf9a6f139c3ed2d122bd4477d527ed5684b
Amanda Hargrove <amandahargrove98@gmail.com>
2022-09-14 14:36:08 -0400

    Signup image bug

--- components/Signup.jsx
+++ components/Signup.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import "./styles/Signup.scss";
-// import "../assets/zoom.jpg";
+// import zoom from "../assets/zoom.jpg";
 
-const Signup = (props) => {
+const Signup = () => {
   return (
     <div>
       <div className="layout-container">
         <div className="signup-container">
-          <h1>Create an Account</h1>
           <div className="form-container">
+            <h1>Create an Account</h1>
             <form action="">
               <input type="text" placeholder="Username" />
               <input type="email" placeholder="Email" />
               <input type="password" placeholder="Password" />
+              <input type="submit" value="Create account" />
+              <input type="submit" value="Connect with Calendly" />
+              <p>
+                Already have an account? <a href="#">Login in</a>
+              </p>
             </form>
           </div>
         </div>
         <div className="login-link-container">
-          <div className="login-text-container">
-            <h3>Already have an account?</h3>
-            <a href="#">Login</a>
-            {/* <img src="zoom.jpg" alt="Man on online meeting" /> */}
+          <div className="dummy-image">
+            <h1>Image</h1>
           </div>
+          {/* <img src={zoom} alt="Man on online meeting" /> */}
         </div>
       </div>
     </div>
