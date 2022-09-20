ba81d9a2cd93a91c546b3d40a7a72122fe73a6a2
Amanda Hargrove <amandahargrove98@gmail.com>
2022-09-20 14:47:07 -0400

    progress on section 2

--- components/App.jsx
+++ components/App.jsx
@@ -7,13 +7,13 @@ import { applyMiddleware, createStore } from "redux";
 import rootReducer from "../reducers/rootReducer";
 import "./styles/App.scss";
 import Navbar from "./Navbar";
-import Login from "./Login";
 import Signup from "./Signup";
 import Dashboard from "./Dashboard";
 import Settings from "./Settings";
 import logger from "redux-logger";
 import LandingNavbar from "./LandingNavbar";
 import CancelMessage from "./CancelMessage";
+import calendar from "../assets/calendar.png";
 
 const App = () => {
   const [login, setLogin] = useState(false);
@@ -32,9 +32,55 @@ const App = () => {
   return (
     <div>
       {login ? <Navbar /> : <LandingNavbar />}
-      {/* <LandingNavbar /> */}
-      <h1 className="content-under-nav">App</h1>
-      <a href="/oauth">Login with Calendly</a>
+      {/* Section 1 */}
+      <div className="landing-section-one">
+        <div className="heading-container">
+          <h1>Secure your Calendly links and let us do the heavy lifting</h1>
+          <h3>
+            Protectly will automatically cancel meetings that are not on your
+            approved list.
+          </h3>
+          <a href="/oauth" className="get-started-link">
+            Get Started
+          </a>
+        </div>
+        <div className="heading-picture-container">
+          <img src={calendar} alt="Calendar illustration" />
+        </div>
+      </div>
+
+      {/* Section 2 */}
+      <div className="landing-section-two">
+        <h1>How Protectly Works</h1>
+        <div className="landing-cards">
+          <div className="card">
+            <h3>Connect</h3>
+            <p>
+              Connect your Calendly Account to Protectly and automatically
+              create your Protectly account.
+            </p>
+          </div>
+          <div className="card">
+            <h3>Authorize</h3>
+            <p>
+              Tell Protectly who is allowed to schedule meetings with you. List
+              out the email addresses and let Protectly do the work.
+            </p>
+          </div>
+          <div className="card">
+            <h3>Rest Easy</h3>
+            <p>
+              Rest easy knowing that Protectly protects your calendar and your
+              Calendly links by automatically canceling meetings with
+              unauthorized users.
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Section three */}
+      <div className="landing-section-three">
+        <h1>Why People Choose Protectly</h1>
+      </div>
     </div>
   );
 };
@@ -48,7 +94,6 @@ window.onload = () => {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<App />} />
-            <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/settings" element={<Settings />} />
